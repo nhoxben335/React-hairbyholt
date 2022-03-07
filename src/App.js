f0a9fe7fa079283c22abd903f94aaad8f5e0ae7b
max <m57morse@gmail.com>
2022-03-07 14:09:55 -0400

    basic components working

--- src/App.js
+++ src/App.js
@@ -3,28 +3,33 @@ import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Services from "./components/Services";
+import Showcase from "./components/Showcase";
 
 function App() {
   return (
-        <React.Fragment>
-        <Helmet>
-          <meta charSet="utf-8"/>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <title>Hair By Holt</title>
-        </Helmet>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/services" element={<Services/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/showcase" element={<Showcase/>}/>
-          <Route path="*" element={<NoMatch/>}/>
-        </Routes>
-        <Footer/>
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Hair By Holt</title>
+      </Helmet>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/showcase" element={<Showcase/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="*" element={<NoMatch/>}/>
+      </Routes>
+      <Footer/>
     </React.Fragment>
   );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../css/reset.css";
+import "../css/footer.css";
+
+const Footer = () => {
+   return (
+      <div>
+         footer
+      </div>
+   );
+}
+
+export default Footer;
