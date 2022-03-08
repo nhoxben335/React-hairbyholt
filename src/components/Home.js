c7691f050bf3d7462b8cfa3c2584888e1615d1b1
max <m57morse@gmail.com>
2022-03-08 10:21:26 -0400

    more crap for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import "../css/reset.css";
 import "../css/home.css";
+import Flickity from "flickity";
 
 const Home = () => {
+   const flickity = new Flickity("", {});
+   
    return (
-      <div>
-         bagel
-      </div>
+      <React.Fragment>
+         <div id="section-1">
+            <img src="https://picsum.photos/1400/600"/>
+         </div>
+         <div id="section-2">
+            <h1>Casual, Affirming, Professional</h1>
+         </div>
+      </React.Fragment>
    );
 }
 
