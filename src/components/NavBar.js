27eaffec01c71a74048149b19661980d3546c2cd
Brooke Grover <brooketg@hotmail.com>
2022-03-08 14:53:21 -0400

    formatting changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import "../css/reset.css";
 import "../css/navbar.css";
@@ -11,15 +11,15 @@ const NavBar = () => {
             <div id="links" className="container-fluid">
                   <a className="navbar-brand"><Link to="" id="title">Hair By Holt</Link></a>
                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false">
-                     <img src={icon} className="navbar-toggler-icon" alt=""/>
+                        <img src={icon} className="navbar-toggler-icon" alt=""/>
                      </button>
                <div className="collapse navbar-collapse justify-content-end" id="menu">
-               <ul id="linkItems" className="nav navbar-nav">
-                  <li classname="nav-item"><Link to="/services" className="link">Services</Link></li>
-                  <li classname="nav-item"><Link to="/contact" className="link">Contact</Link></li>
-                  <li classname="nav-item"><Link to="/showcase" className="link">Showcase</Link></li>
-                  <li classname="nav-item"><Link to="/about" className="link">About</Link></li>
-               </ul>
+                  <ul id="linkItems" className="nav navbar-nav">
+                     <li classname="nav-item"><Link to="/services" className="link">Services</Link></li>
+                     <li classname="nav-item"><Link to="/contact" className="link">Contact</Link></li>
+                     <li classname="nav-item"><Link to="/showcase" className="link">Showcase</Link></li>
+                     <li classname="nav-item"><Link to="/about" className="link">About</Link></li>
+                  </ul>
                </div>
             </div>
          </nav>
