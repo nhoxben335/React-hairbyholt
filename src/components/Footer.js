8a3ce17a840340b056c5877110511648d47643d1
Brooke Grover <brooketg@hotmail.com>
2022-03-09 13:37:06 -0400

    footer :D

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "../css/reset.css";
 import "../css/footer.css";
+import logo from "../images/instagram-logo.png";
 
 const Footer = () => {
    return (
-      <div>
-         footer
+      <div id="footer" className="container-fluid">
+         <div className="row">
+            <div className="col-4"></div>
+            <div className="col-4">
+               <p>Hair by Taylor Holt</p>
+            </div>
+            <div className="col-4 text-end">
+               <a href="https://www.instagram.com/tayloranneholt/?utm_source=ig_embed"><img src={logo} className="float-right"/></a>
+            </div>
+         </div>
       </div>
    );
 }
