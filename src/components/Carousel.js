ccd6d4e5572d70c4fb3a0816d91a3bab892be503
Brooke Grover <brooketg@hotmail.com>
2022-03-09 13:01:19 -0400

    added responsiveness to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -20,6 +20,18 @@ const Carousel = () => {
       speed: 300,
       swipe: true,
       swipeToSlide: true,
+      responsive: [
+         {
+           breakpoint: 1024,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             infinite: true,
+             dots: true,
+             variableWidth: true,
+           }
+         }
+      ]
    };
 
    return (
