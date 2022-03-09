1216c11a7691962cf6c9f196ed7e9dedc704b37a
Brooke Grover <brooketg@hotmail.com>
2022-03-09 13:05:44 -0400

    height stuff

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -30,6 +30,7 @@ const Carousel = () => {
              infinite: true,
              dots: true,
              variableWidth: true,
+             adaptiveHeight: true
            }
          }
       ]
