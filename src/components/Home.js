66390a1e72cecf727a4d94438b383094a6640d26
Nhan Vo <nhoxben335@gmail.com>
2022-03-08 22:36:19 -0400

    Slider with error

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,15 @@ import React from 'react';
 import "../css/reset.css";
 import "../css/home.css";
 import Flickity from "flickity";
+import Slider from "react-slick";
 
 
-// <style>
-// @import url('http://fonts.cdnfonts.com/css/al-ragas');
-// </style>
-
-const Home = () => {
+function Home() {
    // const flickity = new Flickity("../images", {});
-
    return (
       <React.Fragment>
          <div id="section-1">
-            <img src="https://picsum.photos/1400/600"/>
+            <img src="https://picsum.photos/1400/600" />
          </div>
          <div id="section-2">
             <h1>Casual, Affirming, Professional</h1>
@@ -29,5 +25,44 @@ const Home = () => {
       </React.Fragment>
    );
 }
+export default class CenterMode extends Component {
+   render() {
+     const settings = {
+       className: "center",
+       centerMode: true,
+       infinite: true,
+       centerPadding: "60px",
+       slidesToShow: 3,
+       speed: 500
+     };
+     return (
+       <div>
+         <h2>Center Mode</h2>
+         <Slider {...settings}>
+           <div>
+             <h3>1</h3>
+           </div>
+           <div>
+             <h3>2</h3>
+           </div>
+           <div>
+             <h3>3</h3>
+           </div>
+           <div>
+             <h3>4</h3>
+           </div>
+           <div>
+             <h3>5</h3>
+           </div>
+           <div>
+             <h3>6</h3>
+           </div>
+         </Slider>
+       </div>
+     );
+   }
+ }
+
 
 export default Home;
+
