adddec36b32a4f6d124e649cd921e057c09d1832
Nhan Vo <nhoxben335@gmail.com>
2022-03-08 22:16:21 -0400

    Bring this to original state and re-test the slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,7 @@ import React from 'react';
 import "../css/reset.css";
 import "../css/home.css";
 import Flickity from "flickity";
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+
 
 // <style>
 // @import url('http://fonts.cdnfonts.com/css/al-ragas');
@@ -29,47 +28,6 @@ const Home = () => {
          </div>
       </React.Fragment>
    );
-}
-
-// slider 
-import React, { Component } from "react";
-import Slider from "react-slick";
-
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
 }
 
 export default Home;
