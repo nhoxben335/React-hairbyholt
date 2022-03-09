9de8fec2303a76b005ffb2093aa242e98c9d6f72
Brooke Grover <brooketg@hotmail.com>
2022-03-09 13:04:55 -0400

    sdfdfgdfg

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -20,7 +20,19 @@ const Carousel = () => {
       speed: 300,
       swipe: true,
       swipeToSlide: true,
-      variableWidth: true
+      variableWidth: true,
+      responsive: [
+         {
+           breakpoint: 1024,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             infinite: true,
+             dots: true,
+             variableWidth: true,
+           }
+         }
+      ]
    };
 
    return (
