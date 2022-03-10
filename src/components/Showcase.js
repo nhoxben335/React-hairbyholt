7c3c8eeab76819c593fe059f84f29718ae1b7358
Brooke Grover <brooketg@hotmail.com>
2022-03-10 11:31:13 -0400

    added content to showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,8 +4,43 @@ import "../css/showcase.css";
 
 const Showcase = () => {
    return (
-      <div>
-         showcase
+      <div id="showcase">
+         <h1>showcase</h1>
+         <div className="container">
+            <div className="row">
+
+               <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/b_1.jpg') } className="img-fluid"/>
+               </div>
+
+               <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/co_1.jpg')} className="img-fluid"/>
+                  </div>
+
+                  <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/ern_1.jpg')} className="img-fluid"/>
+                  </div>
+
+                  <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/ja_1.jpg')} className="img-fluid"/>
+                  </div>
+
+                  <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/josh_1.jpg')} className="img-fluid"/>
+                  </div>
+
+                  <div className="col-lg-4">
+                  
+                     <img src={require('../images/showcaseImages/ka_1.jpg')} className="img-fluid"/>
+                  </div>
+
+                  <div className="col-lg-4">
+                     <img src={require('../images/showcaseImages/ri_3.jpg')} className="img-fluid"/>
+                  </div>
+
+            </div>
+         </div>
+         
       </div>
    );
 }
