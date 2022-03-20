9a1868c2a4ed1092300c23e21ef9a52adf7180d2
Brooke Grover <brooketg@hotmail.com>
2022-03-20 13:19:30 -0300

    updated map location

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
          <div className="container" id="content">
             <div className="row">
                <div className="col-xl-6 contactmap">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d90836.75450953709!2d-63.63332833772491!3d44.64509900091764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b5a211407dbfac1%3A0x666be3a6438b2ddc!2sHalifax%2C%20NS!5e0!3m2!1sen!2sca!4v1647717312883!5m2!1sen!2sca" 
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.169096942675!2d-73.58325068423157!3d45.526802637284746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91bd07be304dd%3A0xa365c8b7482ac32b!2s4517%20St%20Andre%20St%2C%20Montreal%2C%20QC%20H2J%202Z8!5e0!3m2!1sen!2sca!4v1647793093438!5m2!1sen!2sca" 
                   width="100%" 
                   height="100%" 
                   style={{border:0}} 
