05a85d170452d1ee19518b860a4a49007ce87e30
max <m57morse@gmail.com>
2022-04-04 16:30:49 -0300

    yes

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,11 +4,15 @@ import "../css/showcase.css";
 import Footer from "./Footer";
 import 'animate.css';
 
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
 const Showcase = () => {
    function importAll(r) {
       return r.keys().map(r);
    }
    const imageLocations = importAll(require.context('../images/showcaseImages/', false, /\.(png|jpe?g|svg)$/));
+   const [zoomed, setZoomed] = React.useState(false);
 
    return (
       <div id="showcase">
@@ -19,7 +23,20 @@ const Showcase = () => {
             <div className="row">
                {
                   imageLocations.map((imageLocation) => {
-                     return (<div className="col-lg-4"><img key={imageLocation} src={imageLocation} className="img-fluid" /></div>);
+                     return (
+                        <div className="col-lg-4">
+                           {/* <Image 
+                              key={imageLocation} 
+                              src={imageLocation} 
+                              className="img-fluid" 
+                              // style={{ width: "200px", height: "auto" }} 
+                              zoomed={zoomed} 
+                              onClick={() => setZoomed(true)} 
+                              onRequestClose={() => setZoomed(false)}
+                           /> */}
+                           <Zoom><img overlayBgColorStart="rgba(155, 155, 0, 0.9)" overlayBgColorEnd="rgba(0, 0, 0, 0.9)" wrapStyle={{width: "100%"}} width="auto" height="100%" key={imageLocation} src={imageLocation} /></Zoom>
+                        </div>
+                     );
                   })
                }
             </div>
