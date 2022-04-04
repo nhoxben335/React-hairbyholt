29bb48828e1c04e3a0c73a69163915a9a4a50802
Brooke Grover <brooketg@hotmail.com>
2022-04-04 15:18:17 -0300

    navbar closes AND links work :D

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,26 +7,20 @@ import logo from "../images/instagram-logo.png";
 
 const NavBar = () => {
    return (
-      <div id="nav" data-toggle="collapse" data-target="#menu">
+      <div id="nav">
          <nav className="navbar navbar-expand-lg">
             <div id="links" className="container-fluid">
                   <a className="navbar-brand"><Link to="" id="title">Studio Metamorphose</Link></a>
                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false">
                         <img src={icon} className="navbar-toggler-icon" alt=""/>
                      </button>
-               <div className="collapse navbar-collapse justify-content-end" id="menu">
+               <div className="collapse navbar-collapse justify-content-end" id="menu" data-bs-toggle="collapse" data-bs-target="#menu">
                   <ul id="linkItems" className="nav navbar-nav">
                      {/* <li classname="nav-item"><Link to="/services" className="link">Services</Link></li> */}
-                     <li classname="nav-item">
-                        <Link to="/contact" className="link link-green" data-bs-toggle="collapse" data-bs-target="#menu">Contact</Link>
-                     </li>
-                     <li classname="nav-item">
-                        <Link to="/showcase" className="link link-blue" data-bs-toggle="collapse" data-bs-target="#menu">Showcase</Link>
-                     </li>
-                     <li classname="nav-item">
-                        <Link to="/about" className="link link-pink" data-bs-toggle="collapse" data-bs-target="#menu">About</Link>
-                     </li>
-                     <li classname="nav-item"><a href="https://www.instagram.com/tayloranneholt/?utm_source=ig_embed" data-bs-toggle="collapse" data-bs-target="#menu"><img src={logo} id="instagram"/></a></li>
+                     <li classname="nav-item"><Link to="/contact" className="link link-green"><span data-bs-toggle="collapse" data-bs-target="#menu">Contact</span></Link></li>
+                     <li classname="nav-item"><Link to="/showcase" className="link link-blue"><span data-bs-toggle="collapse" data-bs-target="#menu">Showcase</span></Link></li>
+                     <li classname="nav-item"><Link to="/about" className="link link-pink"><span data-bs-toggle="collapse" data-bs-target="#menu">About</span></Link></li>
+                     <li classname="nav-item"><a href="https://www.instagram.com/tayloranneholt/?utm_source=ig_embed"><img src={logo} id="instagram"/></a></li>
                   </ul>
                </div>
             </div>
